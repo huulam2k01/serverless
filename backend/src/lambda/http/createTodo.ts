d01257a94ccac30fe19a487e8ec42164bfb05c43
huulam2k01 <huulamdo1234@gmail.com>
2023-06-07 11:58:28 +0700

    send mail when create new Todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,40 +1,62 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import { getEmailUser, getUserId } from '../utils'
 import { createTodo } from '../../businessLogic/todos'
+const nodemailer = require('nodemailer') 
 
-export const handler = 
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-      const newTodo: CreateTodoRequest = JSON.parse(event.body)
-      // TODO: Implement creating a new TODO item
-  
-      const userId = getUserId(event)
-  
-      const newItem = await createTodo(newTodo,userId)
-  
-  
-      return {
-        statusCode: 201,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true
-        },
-        body: JSON.stringify({
-          newItem
-        })
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    // TODO: Implement creating a new TODO item
+
+    const userId = getUserId(event)
+
+    const newItem = await createTodo(newTodo, userId)
+
+    const emailUser = getEmailUser(event)
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth:{
+        user: 'huulamdo1234@gmail.com',
+        pass: 'MysecretPassword'
       }
-      
-    } catch (error) {
-      console.log(error);
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true
-        },
-        body: error.message
-      }
+    })
+
+    const mailOptions = {
+      from: 'huulamdo1234@gmail.com',
+      to: emailUser,
+      subject: 'NEW TODO',
+      text: `${newItem.name} need to do`
+    };
+
+    console.log(emailUser)
+
+    const info = await transporter.sendMail(mailOptions);
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        newItem,
+        info
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: error.message
     }
   }
+}
